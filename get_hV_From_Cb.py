f82cfb7116560e12fbc8c71aa3cc83c1d6e4a74b
kush <kush_1901me37@iitp.ac.in>
2021-10-30 15:06:38 +0530

    hv function corrected

--- get_hV_From_Cb.py
+++ get_hV_From_Cb.py
@@ -8,11 +8,14 @@ def fun_hv(x_inp,y_inp):
 
   contact_angle_given = y_inp
   y_new = np.arange(0,3,0.01)
+  rnd=1
+  if int(contact_angle_given)-contact_angle_given==0:
+    rnd=0
 
   min_diff = 99999
   for j in y_new:
     contact_angle_result = contact_angle_predicted(x_new,j)[0]
-    if(round(contact_angle_result,1) == round(contact_angle_given,1)):
+    if(round(contact_angle_result,rnd) == round(contact_angle_given,rnd)):
       diff = abs(contact_angle_given-contact_angle_result)
       if(diff<=min_diff):
         min_diff = diff
